96c3bf62d24f730491bf106dd6520aa75fc18c38
vegasahoo <shivvchandan@gmail.com>
2023-07-30 23:47:50 +0530

    added dockerfile

--- src/main/java/com/satya/mongodb/controller/DataController.java
+++ src/main/java/com/satya/mongodb/controller/DataController.java
@@ -2,6 +2,8 @@ package com.satya.mongodb.controller;
 
 import com.satya.mongodb.model.Product;
 import com.satya.mongodb.repository.service.ProductRepoService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -11,8 +13,16 @@ import java.util.List;
 
 @RestController
 public class DataController {
+
+    private Logger logger = LoggerFactory.getLogger(DataController.class);
     @Autowired
     private ProductRepoService productRepoService;
+
+    @GetMapping("/health-check")
+    public String healthCheck() {
+        logger.info("alive");
+        return "alive";
+    }
 
     @GetMapping("/find-by-code")
     public Product getProduct(@RequestParam String code){
